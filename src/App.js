c4e92147cbf8c3522272a6c72aeae6bbb7e9cfab
Vengada1 <venkatnathan10@gmail.com>
2023-04-05 08:22:36 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { AddUser } from './components/AddUsers';
+import Dashboard from './components/Dashboard';
+import EditUser from './components/EditUsers';
+import { Nopage } from './components/NoPage';
+import UserComponent from './components/UserComponent';
+import { UserDetails } from './components/UserDetails';
+import { data } from './Data/data';
+import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
 
 function App() {
+const [user, setUser] = useState(data)
+  //useEffect
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Switch>
+
+        <Route exact path="/">
+        <UserComponent
+        user={user}
+        setUser={setUser}/>
+        </Route>
+        
+        <Route path="/add/user">
+          <AddUser
+          user={user}
+          setUser={setUser}/>
+        </Route>
+
+        <Route path="/edit/:id">
+            <EditUser
+             user={user}
+             setUser={setUser}
+            />
+        </Route>
+
+        <Route path="/user/:id">
+           <UserDetails user={user}/>
+        </Route>
+
+        
+        <Route path="/dashboard">
+           <Dashboard/>
+        </Route>
+
+
+        <Route path="/students">
+             <Redirect path= "/"/>
+        </Route>
+
+        <Route path = "**">
+            <Nopage/>
+        </Route>
+      
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/Data/data.js
@@ -0,0 +1,58 @@
+export  const data = [
+    {
+        id : "100",
+        name : "Valentino",
+        batch : "B43WD",
+        email: "valentino@gmail.com",
+        experience : 2,
+    },
+    {
+        id : "101",
+        name : "Laliga",
+        batch : "B43WD",
+        email: "laligaa@gmail.com",
+        experience : 1,
+    },
+    {
+        id : "102",
+        name : "Villian",
+        batch : "B43WD",
+        email: "villian@gmail.com",
+        experience : 1,
+    },
+    {
+        id : "103",
+        name : "Velar",
+        batch : "B43WD",
+        email: "velar@gmail.com",
+        experience : 3,
+    },
+    {
+        id : "104",
+        name : "Rossi",
+        batch : "B43WD",
+        email: "rossi@gmail.com",
+        experience : 2,
+    },
+    {
+        id : "105",
+        name : "Vino",
+        batch : "B43WD",
+        email: "vino@gmail.com",
+        experience : 1,
+    },
+    {
+        id : "106",
+        name : "Kaviyan",
+        batch : "B43WD",
+        email: "kaviyan@gmail.com",
+        experience : 2,
+    },
+    {
+        id : "107",
+        name : "Sandhiya",
+        batch : "B43WD",
+        email: "sand@gmail.com",
+        experience : 3,
+    }
+]
